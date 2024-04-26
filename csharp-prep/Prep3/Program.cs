5641897194ac9a27b56181dd8bd8453e6e993543
Jigga Byte <jigga.e10@gmail.com>
2024-04-26 14:07:11 +0100

    added modifications to prep 1 and also added the remaining preps 2 to 5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+
+        string replay = "no";
+
+
+
+        do
+        {
+
+            // decalre magic number and guess initial variables
+            int magicNumber;
+            int guessNumber;
+
+            int guesses = 0;
+
+            // get magic number
+            // Console.Write("What is the magic number? ");
+            // magicNumber = int.Parse(Console.ReadLine());
+
+            // generate random magic number
+            Random randomGenerator = new Random();
+            magicNumber = randomGenerator.Next(1, 100);
+
+            do
+            {
+
+                // get guess number
+                Console.Write("What is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+                guesses += 1;
+
+                if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
+            } while (guessNumber != magicNumber);
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"You made {guesses} guesses.");
+
+
+            Console.Write("Do you want to replay the game? ");
+            replay = Console.ReadLine();
+        } while (replay.ToLower().Equals("yes"));
+
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Console.WriteLine("Hello Prep4 World!");
+
+        // Print welcome info
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // initiate variables
+        int number = -1;
+        float sum = 0;
+        float avg = 0;
+        int maxNumber = 0;
+        List<int> numList = new List<int>();
+
+        while (number != 0)
+        {
+
+            // Prompt user
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numList.Add(number);
+
+        }
+
+        sum = numList.Sum();
+
+        avg = (float)sum/numList.Count;
+
+        maxNumber = numList.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
+
+        numList.Sort();
+        int minPositive = 0;
+
+        foreach(int num in numList)
+        {
+            if(num <= 0)
+            {
+
+            }
+            else
+            {
+                minPositive = num;
+                break;
+            }
+        }
+
+        Console.WriteLine($"The smallest positive number is: {minPositive}");
+        Console.WriteLine("The sorted list is: ");
+        foreach(int num in numList)
+        {
+            Console.WriteLine($"{num}");
+        }
+
+
+
+
     }
 }
